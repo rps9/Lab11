b26da21a599b4c523d4d079c757c27193bad20fc
Ryan Smith <ryans6892@gmail.com>
2023-12-01 11:19:16 -0500

    initial commit

---
+++ Library.cpp
@@ -0,0 +1,26 @@
+//include headers and libraries
+#include "Library.h"
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+class Library
+{   
+    //implement class variables
+    public:
+        Library()
+        {
+            //implement constructor
+        }
+        ~Library()
+        {
+            //implement destructor
+        }
+    private:
+        //implement class variables
+    protected:
+        //implement class variables
+};
+
+//implement class functions
